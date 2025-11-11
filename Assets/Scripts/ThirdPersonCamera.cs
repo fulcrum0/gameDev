c056123e68f43a30ddf9a91c4cebbe651ed50147
tarikerenkurt <tarikerenkurt@gmail.com>
2025-11-11 23:29:58 +0100

    v0.6.1
    UPDATE::
    Camera updated.

--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -2,33 +2,51 @@ using UnityEngine;
 
 public class ThirdPersonCamera : MonoBehaviour {
     [Header("References")]
-    [SerializeField] Rigidbody rb;
+    Rigidbody rb;
     [SerializeField] Transform orientation;
     [SerializeField] Transform player;
-    [SerializeField] Transform playerObj;
+    [SerializeField] Transform playerAnim;
 
     [Header("Settings")]
     [SerializeField] float rotationSpeed;
-
+    void Awake() {
+        rb = GetComponent<Rigidbody>();
+    }
     void Start() {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
     void Update() {
+        HandleCamera();
+    }
 
-        Vector3 viewDir = player.position - transform.position;
+    void HandleCamera() {
 
-        if (viewDir.sqrMagnitude > 0.001f)
-            orientation.forward = viewDir.normalized;
+        Vector3 camForward = transform.forward;
+        camForward.y = 0f;
+        camForward.Normalize();
+
+        Vector3 camRight = transform.right;
+        camRight.y = 0f;
+        camRight.Normalize();
+
+        orientation.forward = camForward;
 
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         float verticalInput = Input.GetAxisRaw("Vertical");
 
-        Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
-        
-        if (inputDir != Vector3.zero)
-            playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
+        Vector3 inputDir = camForward * verticalInput + camRight * horizontalInput;
+
+        if (inputDir.sqrMagnitude > 0.001f) {
+            player.forward = Vector3.Slerp(
+            player.forward,
+            inputDir.normalized,
+            Time.deltaTime * rotationSpeed
+            );
+        }
     }
+
+
 
 }
