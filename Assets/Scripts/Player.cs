a6889c61a59c1261ece8fb64ed6bbbc0e56e857c
tarikerenkurt <tarikerenkurt@gmail.com>
2025-11-15 12:06:46 +0100

    v0.7
    ADDED::
    New player asset and animations added.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,27 +2,57 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     [Header("References")]
+    [SerializeField] Transform orientation;
     Animator anim;
     [Header("Settings")]
-    [SerializeField] float speed;
+    [SerializeField] float movementSpeed;
+    // [SerializeField] float rotationSpeed;
 
     void Awake() {
         anim = GetComponentInChildren<Animator>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     void Update() {
-        HandleWalk();
+        HandleCameraRelativeMovement();
     }
 
-    void HandleWalk() {
+    void LateUpdate() {
+        CameraRotation();
+    }
+
+    void HandleCameraRelativeMovement() {
+        //  get nputs horizontal and vertical
         float xInput = Input.GetAxisRaw("Horizontal");
         float zInput = Input.GetAxisRaw("Vertical");
 
-        Vector3 move = new Vector3(xInput, 0f, zInput).normalized;
-        transform.Translate(speed * Time.deltaTime * move, Space.Self);
+        // get cam directions
+        Vector3 camForward = Camera.main.transform.forward;
+        camForward.y = 0f;
+        camForward.Normalize();
 
-        float currentSpeed = move.magnitude;
+        Vector3 camRight = Camera.main.transform.right;
+        camRight.y = 0f;
+        camRight.Normalize();
 
+        // turn input to the rotation of cam
+        Vector3 moveDir = (camForward * zInput + camRight * xInput).normalized;
+
+        // move
+        if (moveDir.sqrMagnitude > 0.01f) {
+            transform.Translate(moveDir * movementSpeed * Time.deltaTime, Space.World);
+
+            // turn anim
+            // Quaternion targetRot = Quaternion.LookRotation(moveDir);
+            // anim.rotation = Quaternion.Slerp(anim.rotation, targetRot, Time.deltaTime * rotationSpeed);
+        }
+
+        // 6) Animation param
+        float currentSpeed = Mathf.Clamp01(moveDir.magnitude);
         anim.SetFloat("move", currentSpeed);
+    }
+    void CameraRotation() {
+        orientation.rotation = Quaternion.identity;
     }
 }
